cb67745191ee8b658fe15c521d05461f8b705c76
varshil14 <varshilmheshwari19@gmail.com>
2023-08-10 15:14:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var person = new object();
+person ["firstname"]="nani";
+person["lastname"]="viva
+var firstNameproperty="firstname";
+console.log(person);
+console.log(person["firstNameproperty"]);
